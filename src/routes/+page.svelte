7d371aa85fbb0eeebeecbb94308fe553c6d5da50
Hunter Johnston <johnstonhuntera@gmail.com>
2023-02-26 14:23:18 -0500

    starting

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+</script>
+
+<h1>Explain It Like I'm Five</h1>
+<form>
+	<label for="context">Enter the text you want summarized/explained</label>
+	<textarea name="context" rows="5" />
+	<button>Explain it</button>
+	<div class="pt-4">
+		<h2>Explanation:</h2>
+		<p>Explaining text here</p>
+	</div>
+</form>
---
+++ svelte.config.js
@@ -0,0 +1,20 @@
+import preprocess from "svelte-preprocess";
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: [vitePreprocess(), preprocess({
+        postcss: true
+    })],
+
+	kit: {
+		adapter: adapter({
+            runtime: "nodejs18.x",
+        })
+	}
+};
+
+export default config;
