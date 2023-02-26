073da03f1f9ca046b76c1240094f3b0ac5a4ab69
Hunter Johnston <johnstonhuntera@gmail.com>
2023-02-26 14:25:24 -0500

    starting code

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from "svelte-preprocess";
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,9 +11,7 @@ const config = {
     })],
 
 	kit: {
-		adapter: adapter({
-            runtime: "nodejs18.x",
-        })
+		adapter: adapter()
 	}
 };
 
